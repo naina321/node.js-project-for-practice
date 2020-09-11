8cae9e0330ad7781e1cae2e6b79801a73c7c2770
naina321 <naina1910078@akgec.ac.in>
2020-09-11 09:45:49 +0530

    add file and import express module

---
+++ index.js
@@ -0,0 +1,16 @@
+const http =require("http");
+
+const express =require("express");
+
+const app = express();
+
+app.use((req ,res ,next)=> {
+    console.log("in the middleware");
+    next();
+});
+
+app.use((req ,res ,next)=> {
+    console.log(" another middleware");
+    res.send("<h1>hello in express!</h1>");
+});
+app.listen(3000);
